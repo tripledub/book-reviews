110c8fe5bb47312995657faca621bdba30336eea
Stewart Campbell <stewartcuk@gmail.com>
2025-07-29 08:50:53 +0100

    exclude application base files from simplecov

--- spec/support/simplecov_setup.rb
+++ spec/support/simplecov_setup.rb
@@ -8,6 +8,13 @@ SimpleCov.start 'rails' do
   add_filter '/vendor/'
   add_filter '/config/'
 
+  # Exclude application base classes
+  add_filter '/application_job.rb'
+  add_filter '/application_mailer.rb'
+  add_filter '/application_controller.rb'
+  add_filter '/application_record.rb'
+  add_filter '/application_helper.rb'
+
   # Track coverage for specific directories
   add_group 'Models', 'app/models'
   add_group 'Controllers', 'app/controllers'
